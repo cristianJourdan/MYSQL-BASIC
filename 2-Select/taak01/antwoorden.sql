cc22852ef470abbcafa581a12c5d81a00f7fc131
cristianJourdan <2106549@talnet.nl>
2020-05-28 12:14:45 +0200

    select taak01

--- 2-Select/taak01/antwoorden.sql
+++ 2-Select/taak01/antwoorden.sql
@@ -1,13 +1,12 @@
 -- Opdracht 1 - voorbeeld
-SELECT * FROM jaar2016
+SELECT * FROM jaar2016;
 -- Opdracht 2 
-
+SELECT * FROM jaar2015;
 -- Opdracht 3
-
+SELECT `score` FROM `jaar2016` WHERE 1
 -- Opdracht 4
-
+SELECT `regio` FROM `jaar2015` WHERE 1
 -- Opdracht 5
-
+SELECT `region`, `score` FROM `jaar2015` WHERE 1
 -- Opdracht 6
-
--- Opdracht 7
+SELECT `rank`, `score` FROM `jaar2016` WHERE 1
