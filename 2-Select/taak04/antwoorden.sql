30f5645487e9e6a0547b1510788b1cc0cc768118
cristianJourdan <2106549@talnet.nl>
2020-06-03 10:51:54 +0200

    taak 4 h2

--- 2-Select/taak04/antwoorden.sql
+++ 2-Select/taak04/antwoorden.sql
@@ -1,19 +1,20 @@
 -- Opdracht 1 
-
+SELECT Max(wage) As hoogste_loon_fc_Utrecht_speler FROM players WHERE club = "FC Utrecht";
 -- Opdracht 2 
-
+SELECT AVG(wage) AS avg_wage_all_players FROM players
 -- Opdracht 3
-
+SELECT Max(wage) AS Max_wage FROM players WHERE club= 'FC Groningen';
 -- Opdracht 4
-
+SELECT Count(*) As spelers_aantal_Manchestercity_and_ManchesterUnited FROM players WHERE club = "Manchester City" AND club ='Manchester United';
 -- Opdracht 5
-
+SELECT AVG(wage) AS avg_wage FROM players WHERE nationality= 'Netherlands';
 -- Opdracht 6 
-
+SELECT AVG(wage) AS avg_wage_spelers_onder_20 FROM players WHERE nationality= 'Netherlands' AND age < 20;
 -- Opdracht 7 
-
+SELECT AVG(wage) AS avg_wage_spelers_onder_20 FROM players WHERE nationality= 'Netherlands' AND age > 20;
 -- Opdracht 8
-
+SELECT Max(value) As Chelsea_speler_meeste_waarde FROM players WHERE club = "Chelsea";
 -- Opdracht 9
-
+SELECT AVG(age) AS avg_age_all_players FROM players WHERE -1;
 -- Opdracht 10
+SELECT ROUND(AVG(value)) AS avg_value  FROM players WHERE club= 'Liverpool';
